620e41d447b66fb3028c85ca9b110d87c9ce2ed5
Aakash-Pandit <aakashpandit366@gmail.com>
2026-03-16 10:05:57 +0530

    Fix broken import in middleware logger

--- application/middleware_logger.py
+++ application/middleware_logger.py
@@ -1,6 +1,6 @@
 import time
 from starlette.middleware.base import BaseHTTPMiddleware
-from logger import logger
+from application.logger import logger
 
 
 class LoggingMiddleware(BaseHTTPMiddleware):
@@ -8,9 +8,7 @@ class LoggingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
 
         start_time = time.time()
-
         response = await call_next(request)
-
         process_time = time.time() - start_time
 
         logger.info(
